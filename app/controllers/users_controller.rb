6d8c0ff351f9df3bf33676f753002d389e9e3087
Daniel Harris <bobth3bum@gmail.com>
2015-02-12 14:14:44 -0700

    add current user to application controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,4 +5,7 @@ class UsersController < ApplicationController
   end
 
   def create
-    
+    @user = User.new(user_params)
+    if @user.save
+      flash[:notice] = 'User was successfully created.'
+      
